af2d2f92d77d6792138813b5662f0f944a22d91f
Chad Timmins <chadtimmins@gmail.com>
2018-08-06 20:49:08 -0700

    s3 Utils (#5)
    * s3 Utils
    
    * fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'aws-python-utils'
+NAME = 'aws_python_utils'
 DESCRIPTION = 'Simplify use of AWS resources in your code with aws-python-utils'
 URL = 'https://github.com/sepulworld/aws-python-utils'
 EMAIL = 'zane.williamson@gmail.com'
