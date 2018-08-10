b185119f9e78e495f6f7fb92e7590cfc4639c1c1
zane <zane.williamson@gmail.com>
2018-08-09 21:44:41 -0700

    pop8 adjustments

--- aws_python_utils/s3.py
+++ aws_python_utils/s3.py
@@ -6,10 +6,8 @@ from io import BytesIO, StringIO
 s3_client = boto3.client("s3")
 LOG = logger.getLogger("S3Util")
 
-"""
-returns tuple representing (bucket,key)
-"""
 def get_bucket_and_key_from_s3_path(s3_path):
+    """returns tuple representing (bucket,key)"""
     res = re.search("s3://(.*?)/(.*)", s3_path)
     if not res:
         # try without s3 protocol
@@ -19,10 +17,8 @@ def get_bucket_and_key_from_s3_path(s3_path):
 
     return res.group(1),res.group(2)
 
-"""
-returns a panda DataFrame
-"""
 def download_s3_file(s3_path, header=None, sep=',', dtype=None, index=None):
+    """returns a panda DataFrame"""
     LOG.info("downloading %s" % s3_path)
     bucket,key = get_bucket_and_key_from_s3_path(s3_path)
     obj = s3_client.get_object(Bucket=bucket, Key=key)
@@ -33,10 +29,8 @@ def download_s3_file(s3_path, header=None, sep=',', dtype=None, index=None):
     return df
 
 
-"""
-uploads contents in io_buffer.  should be instance of StringIO()
-"""
 def upload_to_s3(s3_out_path, io_buffer):
+    """uploads contents in io_buffer.  should be instance of StringIO()"""
     LOG.info("uploading to %s" % s3_out_path)
     bucket,key = get_bucket_and_key_from_s3_path(s3_out_path)
     s3_client.put_object(Bucket=bucket, Key=key, Body=io_buffer.getvalue())
