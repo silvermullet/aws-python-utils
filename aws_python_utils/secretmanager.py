9f7d7b7e19341e50a545bc594260434b54665fc2
Zane Williamson <sepulworld@gmail.com>
2018-08-10 18:44:28 -0700

    pep8 adjustments (#7)
    * pop8 adjustments
    
    * move s3 functions into a class
    
    * class update with init
    
    * Adding self param
    
    * Update function doc
    
    * Adding __init__ for secretmanager

--- aws_python_utils/secretmanager.py
+++ aws_python_utils/secretmanager.py
@@ -1,44 +1,48 @@
 import boto3
 import os
+from . import logger
 from botocore.exceptions import ClientError
+
 
 class AwsSecretManager():
     """AWS Secret Manager class"""
 
-    def get_secret(self,
-                   secret_key=None,
-                   endpoint_url="https://secretsmanager.us-west-2.amazonaws.com",
-                   region_name="us-west-2"):
+    def __init__(self, profile_name=None,
+                 endpoint_url="https://secretsmanager.us-west-2.amazonaws.com",
+                 region_name="us-west-2"):
+        self.session = boto3.Session(profile_name=profile_name)
+        self.endpoint_url = endpoint_url
+        self.region_name = region_name
+        self.secretmanager_client = self.session.client(
+            service_name='secretsmanager',
+            region_name=self.region_name,
+            endpoint_url=self.endpoint_url
+            )
+        self.LOG = logger.getLogger("SecretManagerUtil")
+
+    def get_secret(self, secret_key=None, endpoint_url, region_name):
         """AWS secret get_secret"""
-        
+
         if secret_key == None:
             self.secret_key = os.environ.get("AWS_SECRET_MANAGER_SECRET", None)
         else:
             self.secret_key = secret_key
         self.secret_value = None
         self.binary_secret_data = None
-        self.endpoint_url = endpoint_url
-        self.region_name = region_name
-
-        session = boto3.session.Session()
-        client = session.client(
-            service_name='secretsmanager',
-            region_name=self.region_name,
-            endpoint_url=self.endpoint_url
-        )
 
         try:
-            get_secret_value_response = client.get_secret_value(
+            get_secret_value_response = self.secretmanager_client.get_secret_value(
                 SecretId=self.secret_key
             )
 
         except ClientError as e:
             if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                print("The requested secret " + self.secret_key + " was not found")
+                self.LOG.error(
+                    f'The requested secret {self.secret_key} was not found')
             elif e.response['Error']['Code'] == 'InvalidRequestException':
-                print("The request was invalid due to:", e)
+                self.LOG.error(f'The request was invalid due to: {e}')
             elif e.response['Error']['Code'] == 'InvalidParameterException':
-                print("The request had invalid params:", e)
+                self.LOG.error(f'The request had invalid params: {e}')
         else:
             # Decrypted secret using the associated KMS CMK
             # Depending on whether the secret was a string or binary,
